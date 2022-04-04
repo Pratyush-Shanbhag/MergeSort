13fca1314bc48ffc26f8b4b256b11fb8b2b2bfe4
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-04-03 18:33:02 -0700

    added MergeSort.h and MergeSort.cpp

---
+++ MergeSort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "MergeSort.h"
+
+using namespace std;
+
+void MergeSort::mergeSort() {
+
+}
+
+void MergeSort::sort() {
+    mergeSort();
+}
---
+++ MergeSort.h
@@ -0,0 +1,13 @@
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+#include <string>
+#include "Sort.h"
+
+class MergeSort: public Sort {
+    public:
+        void mergeSort();
+        void sort();
+};
+
+#endif
