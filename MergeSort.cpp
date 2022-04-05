66c40da0fb833692fc68e69a07955079e116efb3
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-04-05 11:58:01 -0700

    finished mergeSort()

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -8,9 +8,14 @@ void MergeSort::merge() {
 }
 
 void MergeSort::mergeSort(int lindex, int rindex) {
-
+    if(lindex < rindex) {
+        int midindex = (lindex + rindex) / 2;
+        mergeSort(lindex, midindex);
+        mergeSort(midindex + 1, rindex);
+        merge();
+    }
 }
 
 void MergeSort::sort() {
-    
+
 }
